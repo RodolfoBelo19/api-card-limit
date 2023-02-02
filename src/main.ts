885f659cf9fcc2f1f0e4430889d356e675d64b98
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2023-02-02 00:26:07 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  });
   await app.listen(3005);
 }
 bootstrap();
