b2aa8ab882a19f7cdcc9da8ed5615bf8ee136227
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2023-02-10 11:28:09 -0300

    fix api

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
+    origin: true,
     credentials: true,
   });
   await app.listen(3005);
