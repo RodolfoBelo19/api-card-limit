394ebf53ed4579bd3016df7354475fb699805c69
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2023-04-06 22:22:59 -0300

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,16 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('tests')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3005);
 }
 bootstrap();
